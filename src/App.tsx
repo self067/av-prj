eb9329caf34577ef91990f4026d12f33b25cd2cd
self067 <seltor@mail.ru>
2020-08-24 22:25:38 +0300

    feat: add NavBar Lesson03

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import Navbar from './components/Navbar';
+// box-sizing: content-box | border-box
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  outline: none;
+}
+
+body {
+  margin: 0;
+  background-color: #f0f0f0;
+  font-family: Roboto, sans-serif;
+  color: black;
+  font-size: 20px;
+    
+}
+
+img {
+  max-width: 100%;
+  height:auto;
+}
+
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+a { 
+  text-decoration: none;
+  color: inherit;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+h1, h2, h3 {
+  font-family: Pacifico;
+  padding: 0;
+  margin: 0;
+}
+
+p {
+    font-family: Pacifico;
+    padding: 0;
+    margin: 0;
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <Navbar />
+        {/* <h1>Hello</h1> */}
+      </div>
+
+    </>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../images/logo.svg';
+import sign from '../images/sign.svg';
+
+const NavbarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #299B01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+ `;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const Login = styled.button`
+  background-color: transparent;
+  color: white; //#299B01;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: none;
+  font-size: 18px;
+  
+`;
+
+const Sign = styled.img`
+  margin-bottom: 5px;
+`;
+
+const Navbar = () => (
+  <NavbarStyled>
+    <Logo>
+      <ImgLogo src={logo} alt="logo" />
+      <H1>MrDonald</H1>
+    </Logo>
+    <Login>
+      <Sign src={sign} />
+      Войти
+    </Login>
+  </NavbarStyled>
+);
+
+export default Navbar;
